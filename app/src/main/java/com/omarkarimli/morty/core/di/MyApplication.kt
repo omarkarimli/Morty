6a99e66bee9fadb10cec0d402f16796c795241ca
Omar Karimli <omarkarimli.361@gmail.com>
2025-11-18 22:50:29 +0400

    update

--- app/src/main/java/com/omarkarimli/morty/core/di/MyApplication.kt
+++ app/src/main/java/com/omarkarimli/morty/core/di/MyApplication.kt
@@ -1,7 +1,7 @@
-package com.omarkarimli.morty
+package com.omarkarimli.morty.core.di
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication:Application()
+class MyApplication: Application()
