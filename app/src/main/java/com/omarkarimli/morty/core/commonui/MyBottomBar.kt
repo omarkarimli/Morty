d249c74d421ee56347c2936b5f305d9fcc3b47ba
Omar Karimli <omarkarimli.361@gmail.com>
2025-11-30 17:47:58 +0400

    update

--- app/src/main/java/com/omarkarimli/morty/core/commonui/MyBottomBar.kt
+++ app/src/main/java/com/omarkarimli/morty/core/commonui/MyBottomBar.kt
@@ -1,12 +1,24 @@
 package com.omarkarimli.morty.core.commonui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.SmallFloatingActionButton
-import androidx.compose.material3.Text
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.omarkarimli.morty.core.navigation.NavDestination
+import com.omarkarimli.morty.ui.theme.Dimens
 
 @Composable
 fun MyBottomBar(
@@ -19,38 +31,70 @@ fun MyBottomBar(
         NavDestination.Episodes
     )
 
-    NavigationBar {
-        navigationItems.forEach { destination ->
-            val isActive = when (destination.route) {
-                NavDestination.Home.route -> currentRoute == destination.route ||
-                        currentRoute.contains("character_details") ||
-                        currentRoute.contains("character_episodes")
-                NavDestination.Episodes.route -> currentRoute == destination.route
-                NavDestination.Dynamic.route -> currentRoute == destination.route
-                else -> currentRoute == destination.route
-            }
+    Surface(
+        color = MaterialTheme.colorScheme.surface,
+        tonalElevation = 3.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            navigationItems.forEach { destination ->
+                val isActive = when (destination.route) {
+                    NavDestination.Home.route -> currentRoute == destination.route ||
+                            currentRoute.contains("character_details") ||
+                            currentRoute.contains("character_episodes")
+                    NavDestination.Episodes.route -> currentRoute == destination.route
+                    NavDestination.Dynamic.route -> currentRoute == destination.route
+                    else -> currentRoute == destination.route
+                }
 
-            if (destination == NavDestination.Dynamic) {
-                SmallFloatingActionButton(
-                    onClick = { onNavigationClick(destination.route) }
+                val containerColor = if (destination == NavDestination.Dynamic) {
+                    MaterialTheme.colorScheme.onSurface
+                } else {
+                    if (isActive) {
+                        MaterialTheme.colorScheme.primaryContainer
+                    } else {
+                        MaterialTheme.colorScheme.surface
+                    }
+                }
+
+                val contentColor = if (destination == NavDestination.Dynamic) {
+                    MaterialTheme.colorScheme.surface
+                } else {
+                    if (isActive) {
+                        MaterialTheme.colorScheme.onPrimaryContainer
+                    } else {
+                        MaterialTheme.colorScheme.onSurface
+                    }
+                }
+
+                FilledTonalIconButton(
+                    modifier = Modifier
+                        .padding(
+                            start = Dimens.dp8,
+                            end = Dimens.dp8,
+                            top = Dimens.dp8,
+                            bottom = Dimens.dp24
+                        )
+                        .width(Dimens.dp48)
+                        .height(Dimens.dp32),
+                    onClick = { onNavigationClick(destination.route) },
+                    shape = IconButtonDefaults.filledShape,
+                    colors = IconButtonDefaults.filledIconButtonColors(
+                        containerColor = containerColor,
+                        contentColor = contentColor
+                    )
                 ) {
                     Icon(
-                        destination.icon,
+                        modifier = Modifier.size(Dimens.dp24),
+                        imageVector = destination.icon,
                         contentDescription = destination.title,
                     )
                 }
-            } else {
-                NavigationBarItem(
-                    icon = {
-                        Icon(
-                            destination.icon,
-                            contentDescription = destination.title,
-                        )
-                    },
-                    label = { Text(destination.title) },
-                    selected = isActive,
-                    onClick = { onNavigationClick(destination.route) },
-                )
             }
         }
     }
--- app/src/main/java/com/omarkarimli/morty/core/navigation/NavDestination.kt
+++ app/src/main/java/com/omarkarimli/morty/core/navigation/NavDestination.kt
@@ -3,12 +3,12 @@ package com.omarkarimli.morty.core.navigation
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material.icons.rounded.SaveAlt
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.omarkarimli.morty.core.constants.Constants
 
 sealed class NavDestination(val title: String, val route: String, val icon: ImageVector) {
     object Home : NavDestination(title = Constants.NAV_DESTINATION_HOME, route = "home_screen", icon = Icons.Filled.Home)
     object Episodes : NavDestination(title = Constants.NAV_DESTINATION_EPISODES, route = "episodes", icon = Icons.Filled.PlayArrow)
-    object Dynamic : NavDestination(title = Constants.NAV_DESTINATION_DYNAMIC, route = "dynamic_download", icon = Icons.Rounded.SaveAlt)
+    object Dynamic : NavDestination(title = Constants.NAV_DESTINATION_DYNAMIC, route = "dynamic_download", icon = Icons.Rounded.Add)
 }
