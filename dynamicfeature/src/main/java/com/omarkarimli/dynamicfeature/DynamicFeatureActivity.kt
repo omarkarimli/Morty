ecd6e55404526f6e8116ff867b6752449bd6694f
Omar Karimli <omarkarimli.361@gmail.com>
2025-11-30 15:03:40 +0400

    update

--- dynamicfeature/src/main/java/com/omarkarimli/dynamicfeature/DynamicFeatureActivity.kt
+++ dynamicfeature/src/main/java/com/omarkarimli/dynamicfeature/DynamicFeatureActivity.kt
@@ -4,12 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import com.omarkarimli.dynamicfeature.ui.theme.MortyTheme
+import com.omarkarimli.morty.R
+import com.omarkarimli.morty.core.commonui.MyTopBar
+import com.omarkarimli.morty.core.commonui.WideButton
+import com.omarkarimli.morty.ui.theme.AppTypography
+import com.omarkarimli.morty.ui.theme.Dimens
 
 class DynamicFeatureActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,14 +36,64 @@ class DynamicFeatureActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MortyTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    Text(
-                        modifier = Modifier.padding(innerPadding),
-                        text = "Dynamic Feature Module"
+                DynamicFeatureScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun DynamicFeatureScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            MyTopBar(
+                title = "Dynamic Feature",
+                showBackButton = true,
+                onBackClick = {}
+            )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(Dimens.dp16),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "New Features Here",
+                style = AppTypography.headlineMedium,
+                textAlign = TextAlign.Center
+            )
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(MaterialTheme.shapes.large),
+                painter = painterResource(id = R.drawable.i1),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(Dimens.dp16)
+            ) {
+                IconButton(
+                    onClick = { }
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Done,
+                        contentDescription = null
                     )
                 }
+                WideButton(
+                    modifier = Modifier.weight(1f),
+                    text = "Completed",
+                    onClick = {},
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
             }
         }
     }
